f39129018be9989b1826a9af96592fb810954700
EJ <ej@ejdohmen.com>
2021-10-15 22:31:07 -0700

    Adding another test

--- tests/main.cpp
+++ tests/main.cpp
@@ -7,13 +7,37 @@
 using namespace posix_utils;
 
 
-TEST(Shared_Ring_Buffer, multiple_write_to_buffer) 
+TEST(Shared_Ring_Buffer, multiple_write_to_same_buffer) 
 {
     std::unique_ptr<Shared_Ring_Buffer> srb_ptr = Shared_Ring_Buffer::create(2, 1);
 
-    strcpy(srb_ptr->getWritePointer(), "A");
-    strcpy(srb_ptr->getWritePointer(), "B");
+    for(unsigned i = 0; i < 100; ++i)
+        strcpy(srb_ptr->getWritePointer(), "A");
 
+    strcpy(srb_ptr->getWritePointer(), "B");
+    srb_ptr->releaseWritePointer();
+
+    char* data = srb_ptr->getReadPointer();
+
+    EXPECT_TRUE(data[0] == 'B');
+}
+
+TEST(Shared_Ring_Buffer, multiple_read_from_same_buffer) 
+{
+    std::unique_ptr<Shared_Ring_Buffer> srb_ptr = Shared_Ring_Buffer::create(2, 2);
+
+    strcpy(srb_ptr->getWritePointer(), "A");
+    srb_ptr->releaseWritePointer();
+    strcpy(srb_ptr->getWritePointer(), "B");
+    srb_ptr->releaseWritePointer();
+
+    char* data = srb_ptr->getReadPointer();
+    EXPECT_TRUE(data[0] == 'A');
+    data = srb_ptr->getReadPointer();
+    EXPECT_TRUE(data[0] == 'A');
+    srb_ptr->releaseReadPointer();
+
+    EXPECT_TRUE(srb_ptr->getReadPointer()[0] == 'B');
 }
 
 TEST(Shared_Ring_Buffer, multithredded1) 
